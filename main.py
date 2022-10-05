2bfda0235d1831f76ff38388e96653d53aa1cf86
Max2288 <kennySRU@ya.ru>
2022-10-05 14:35:18 +0300

    Добавлен основной файл

---
+++ main.py
@@ -0,0 +1,11 @@
+"Illustrate programs."
+from functions import opredelitel_1_poryadka
+from functions import opredelitel_2_poryadka
+from functions import opredelitel_3_poryadka
+from functions import fib
+
+
+print(opredelitel_1_poryadka([[1]]))
+print(opredelitel_2_poryadka([[1, 3], [5, 4]]))
+print(opredelitel_3_poryadka([[15, -7,  5], [-1, 4,  4], [3, 6, 9]]))
+print(fib(1000000))
