87f4fdad3f6f42952ecccbe85310749fd58f5380
Max2288 <kennySRU@ya.ru>
2022-10-06 09:43:37 +0300

    Тесты оформлены по flake8

--- test_main.py
+++ test_main.py
@@ -20,57 +20,83 @@ with open('testforopredelitel1.txt', 'r') as file_opr_first:
 
 
 @pytest.mark.parametrize('test_inp, expect', test_for_opr_first)
-def test_determinant_1(test_inp, expect):
+def test_determinant_first(test_inp: list, expect: int):
+    """Found determinant.
+
+    Args:
+        test_inp (list): test input for function.
+        expect (int): test expecpection for function.
+    """
     assert opredelitel_1_poryadka(test_inp) == expect
 
 
-test_for_opr_2 = []
-with open('testforopredelitel2.txt', 'r') as file_opr_2:
-    new_lst = file_opr_2.readlines()
-    for item in new_lst:
-        list_of_str = [i for i in item.split(':')]
-        list_of_str[0] = list_of_str[0].replace(
-            ',', '').replace('[', '').replace(']', '').split()
-        list_of_str[0] = [int(i) for i in list_of_str[0]]
-        item = ([[list_of_str[0][i], list_of_str[0][i + 1]]\
-                 for i in range(0, len(list_of_str[0]), 2)],\
+test_for_opr_second = []
+with open('testforopredelitel2.txt', 'r') as file_opr_second:
+    new_lst = file_opr_second.readlines()
+    for string in new_lst:
+        list_of_str = []
+        for elem in string.split(':'):
+            list_of_str.append(elem)
+        list_of_str[0] = list_of_str[0].replace(',', '').replace('[', '').replace(']', '').split()
+        list_of_str[0] = [int(new_elem) for new_elem in list_of_str[0]]
+        string = ([[list_of_str[0][dig], list_of_str[0][dig + 1]]\
+                 for dig in range(0, len(list_of_str[0]), 2)],\
                 int(list_of_str[1].replace('\n', '')))
-        test_for_opr_2.append(item)
+        test_for_opr_second.append(string)
 
 
-@pytest.mark.parametrize('test_inp, expect', test_for_opr_2)
-def test_determinant_2(test_inp, expect):
+@pytest.mark.parametrize('test_inp, expect', test_for_opr_second)
+def test_determinant_second(test_inp: list, expect: int):
+    """Found determinant.
+
+    Args:
+        test_inp (list): test input for function.
+        expect (int): test expecpection for function.
+    """
     assert opredelitel_2_poryadka(test_inp) == expect
 
 
-test_for_opr_3 = []
-with open('testforopredelitel3.txt', 'r') as file_opr_3:
-    new_lst = file_opr_3.readlines()
-    for item in new_lst:
-        list_of_str = [i for i in item.split(':')]
-        list_of_str[0] = list_of_str[0].replace(
-            ',', '').replace('[', '').replace(']', '').split()
-        list_of_str[0] = [int(i) for i in list_of_str[0]]
-        item = ([[list_of_str[0][i], list_of_str[0][i + 1], list_of_str[0][i + 2]]\
-                 for i in range(0, len(list_of_str[0]), 3)],\
+test_for_opr_third = []
+with open('testforopredelitel3.txt', 'r') as file_opr_third:
+    new_lst = file_opr_third.readlines()
+    for string in new_lst:
+        list_of_str = []
+        for element in string.split(':'):
+            list_of_str.append(element)
+        list_of_str[0] = list_of_str[0].replace(',', '').replace('[', '').replace(']', '').split()
+        list_of_str[0] = [int(new_elem) for new_elem in list_of_str[0]]
+        string = ([[list_of_str[0][dig], list_of_str[0][dig + 1], list_of_str[0][dig + 2]]\
+                 for dig in range(0, len(list_of_str[0]), 3)],\
                 int(list_of_str[1].replace('\n', '')))
-        test_for_opr_3.append(item)
+        test_for_opr_third.append(string)
 
 
-@pytest.mark.parametrize('test_inp, expect', test_for_opr_3)
-def test_determinant_3(test_inp, expect):
+@pytest.mark.parametrize('test_inp, expect', test_for_opr_third)
+def test_determinant_third(test_inp: list, expect: int):
+    """Found determinant.
+
+    Args:
+        test_inp (list): test input for function.
+        expect (int): test expecpection for function.
+    """
     assert opredelitel_3_poryadka(test_inp) == expect
 
 
 test_for_fib = []
 with open('testfib.txt', 'r') as file_fib:
     test_for_fib = file_fib.readlines()
-    for index in range(len(test_for_fib)):
+    for index, _ in enumerate(test_for_fib):
         test_for_fib[index] = test_for_fib[index].replace('\n', '')
-        list_of_str = [int(i) for i in test_for_fib[index].split(':')]
+        list_of_str = [int(new_elem) for new_elem in test_for_fib[index].split(':')]
         test_for_fib[index] = (list_of_str[0], list_of_str[1])
 
 
 @pytest.mark.parametrize('test_inp, expect', test_for_fib)
-def test_fib(test_inp, expect):
+def test_fib(test_inp: int, expect: int):
+    """Test for fibonacci numbers.
+
+    Args:
+        test_inp (int): test input for function.
+        expect (int): test expecpection for function.
+    """
     assert fib(test_inp) == expect
